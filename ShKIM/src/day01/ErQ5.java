f823c3dd59c6d1ab42f1a8da70696ce0c1169f84
SOHEE\Kim Sohee <piia_@naver.com>
2021-05-30 23:48:25 +0900

    210530 5번문제

---
+++ ShKIM/src/day01/ErQ5.java
@@ -0,0 +1,27 @@
+package day01;
+
+public class ErQ5 {
+
+	public static void main(String[] args) {
+		// 정수 n개가 주어졌을 때
+		// n개의 합을 구하는 함수 작성
+
+		int[] test = { 1, 10, 2999989 };
+
+		long num = sum(test);
+
+		System.out.println(num);
+
+	}
+
+	static long sum(int[] a) {
+		long result = 0;
+
+		for (int i = 0; i < a.length; i++) {
+			result += a[i];
+		}
+
+		return result;
+	}
+
+}
