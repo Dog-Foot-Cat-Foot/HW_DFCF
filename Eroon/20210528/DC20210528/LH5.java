68d9cf2e43f973c078a6a8dd24069745c1358284
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-06-01 20:02:35 +0900

    20210601

--- Eroon/20210528/DC20210528/LH5.java
+++ Eroon/20210528/DC20210528/LH5.java
@@ -4,6 +4,27 @@ import java.util.Scanner;
 
 public class LH5 {
 	public static void main(String[] args) {
-	
+		Scanner scan = new Scanner(System.in);
+		
+		int[] test = new int[3];
+		
+		for(int i = 0; i<test.length; i++) {
+			test[i]=scan.nextInt();
+		}
+
+		long num = sum(test);
+
+		System.out.println(num);
+
+	}
+
+	static long sum(int[] a) {
+		long result = 0;
+
+		for (int i = 0; i < a.length; i++) {
+			result += a[i];
+		}
+
+		return result;
 	}
 }
--- Eroon/20210528/DC20210528/MJ1.java
+++ Eroon/20210528/DC20210528/MJ1.java
@@ -8,6 +8,26 @@ public class MJ1 {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("100보다 작은 정수를 입력하세요. ");
 		int n = scan.nextInt();
+		int a = 1;
+		int b = 1;
+		
+		for(int i=1; i<=n; i++) {
+			if(i>10) {
+				a = i/10;
+				b = i%10;
+			}else {
+				b=i;
+			}
+			
+			if((a==3||a==6||a==9) && (b==3||b==6||b==9)){
+				System.out.print("XX ");
+			}else if(a==3||a==6||a==9||b==3||b==6||b==9) {
+				System.out.print("X ");
+			}else {
+				System.out.print(i+" ");
+			}
+		}
+		
 	
 
 
