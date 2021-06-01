f5384fd26d6c02d050467824cbb31eb52d03dd90
eunji03 <84062291+eunji03@users.noreply.github.com>
2021-06-01 17:05:06 +0900

    Update ej.java

--- EunJi/2021.05.28/ej.java
+++ EunJi/2021.05.28/ej.java
@@ -28,7 +28,39 @@ public class IFEx1 {
 
 
 ## 2 
+import java.util.Scanner;
 
+public class WhileEx1 {
+
+	public static void main(String[] args) {
+		//A+B
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하세요 : ");
+		int n = scanner.nextInt();
+		
+		int a,b;
+		int arr[] = new int[n];
+
+		
+		while(0<=n&&n<=99) {
+			for (int i =  0; i < n; i++) {
+				
+				a = scanner.nextInt();
+				b = scanner.nextInt();
+				
+				arr[i] = a + b;	
+			}
+			
+			for(int i = 0; i < arr.length; i++) {
+				System.out.println("Case #"+(i+1)+": "+arr[i]);
+			}
+			
+		}
+
+	}
+
+}
 
 ## 3
 
