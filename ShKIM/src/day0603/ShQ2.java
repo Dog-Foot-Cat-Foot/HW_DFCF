5fd0126fd2adce9c9cb862d72d381d1c2fa155ae
SH Kim <82145134+shkim-123@users.noreply.github.com>
2021-06-05 00:34:59 +0900

    210605 체육복

--- ShKIM/src/day0603/ShQ2.java
+++ ShKIM/src/day0603/ShQ2.java
@@ -1,5 +1,9 @@
 package day0603;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ShQ2 {
 
 	public static void main(String[] args) {
@@ -11,59 +15,147 @@ public class ShQ2 {
 		// reserve, 1 <= length <= n, 중복 x
 		// 여벌 있는 학생이 도난 당했을 수 있음, 남은 체육복 1개니까 못 빌려줌
 
-		int[] a = { 5, 4, 3, 2, 1 };
-		int[] b = { 3, 1, 2, 5, 4 };
+		int[] a = { 1, 2, 3};
+		int[] b = { 2, 3, 4};
 
-		System.out.println(solution(10, a, b));
+		System.out.println(solution(5, a, b));
 
 	}
+	
+//	public static int solution(int n, int[] lost, int[] reserve) {
+//		int result = n - lost.length;
+//		int num = reserve.length;
+//		int size = 0, size2 = 0;
+//		
+//		for(int l : lost) {
+//			size = 0;
+//			for(int r : reserve) {
+//				if(num > 0 && l != 0 && r!= 0 && l == r) {
+//					result++;
+//					num--;
+//					lost[size2] = 0;
+//					reserve[size] = 0;
+//					break;
+//				} 
+//				size++;
+//			}
+//			size2++;
+//		}
+//		
+//		
+//		
+//		for(int l : lost) {
+//			for(int r : reserve) {
+//				if(num > 0 && r!= 0 && l != 0 && (l-1 == r || l+1 == r)) {
+//					result++;
+//					num--;
+//					break;
+//				}
+//			}
+//		}
+//		
+//		return result;
+//	}
+
+	
 
 	public static int solution(int n, int[] lost, int[] reserve) {
-		// 체육복 있는 학생 수
 		int result = n - lost.length;
-
-		// 체육복 개수
 		int num = reserve.length;
-
-		// 빌려줄 수 있는 학생 수 구하기
-		for (int i = 0; i < lost.length; i++) {
-
-			// 체육복 없으면
-			if (num == 0) {
-				break;
-			}
-
-			// 중복 요소 제거
-			for (int j = 0; j < reserve.length; j++) {
-
-				if (lost[i] == reserve[j]) {
-					reserve[j] = -2;
+		int size = 0, size2 = 0;
+		
+		List<Integer> lostArray = new ArrayList<>();
+		List<Integer> reserveArray = new ArrayList<>();
+		
+		Arrays.stream(lost).sorted().forEach( c -> lostArray.add(c));
+		Arrays.stream(reserve).sorted().forEach( c -> reserveArray.add(c));
+		
+		for(int l : lostArray) {
+			size = 0;
+			for(int r : reserveArray) {
+				if(num > 0 && l !=0 && r != 0 && l == r) {
 					result++;
 					num--;
+					lostArray.set(size2, 0);
+					reserveArray.set(size, 0);
 					break;
-				}
-
+				} 
+				size++;
 			}
-
-			// 체육복 없으면
-			if (num == 0) {
-				break;
-			}
-
-			// 중복 요소 제거
-			for (int j = 0; j < reserve.length; j++) {
-
-				if ((lost[i] - 1 == reserve[j]) || (lost[i] + 1 == reserve[j])) {
-					reserve[j] = -2;
-					result++;
-					num--;
-					break;
-				}
-			} // for 끝
-
+			size2++;
 		}
-
+		
+		lostArray.remove(Integer.valueOf(0));
+		reserveArray.remove(Integer.valueOf(0));
+		
+		size2 = 0;
+		
+		for(int l : lostArray) {
+			size = 0;
+			for(int r : reserveArray) {
+				if(num > 0 && l !=0 && r != 0 && (l-1 == r || l+1 == r)) {
+					result++;
+					num--;
+					lostArray.set(size2, 0);
+					reserveArray.set(size, 0);
+					break;
+				}
+				size++;
+			}
+			size2++;
+		}
+		
 		return result;
 	}
+	
+	
+
+//	public static int solution(int n, int[] lost, int[] reserve) {
+//		// 체육복 있는 학생 수
+//		int result = n - lost.length;
+//
+//		// 체육복 개수
+//		int num = reserve.length;
+//
+//		// 빌려줄 수 있는 학생 수 구하기
+//		for (int i = 0; i < lost.length; i++) {
+//
+//			// 체육복 없으면
+//			if (num == 0) {
+//				break;
+//			}
+//
+//			// 중복 요소 제거
+//			for (int j = 0; j < reserve.length; j++) {
+//
+//				if (lost[i] == reserve[j]) {
+//					reserve[j] = -2;
+//					result++;
+//					num--;
+//					break;
+//				}
+//
+//			}
+//
+//			// 체육복 없으면
+//			if (num == 0) {
+//				break;
+//			}
+//
+//			// 중복 요소 제거
+//			for (int j = 0; j < reserve.length; j++) {
+//
+//				if ((lost[i] - 1 == reserve[j]) || (lost[i] + 1 == reserve[j])) {
+//					reserve[j] = -2;
+//					result++;
+//					num--;
+//					break;
+//				}
+//			} // for 끝
+//
+//		}
+//
+//		return result;
+//	}
 
 }
