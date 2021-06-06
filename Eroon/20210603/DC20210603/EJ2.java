3ce6ba4025f1fdca198d99ed2002c587b2644ba3
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-06-06 15:08:30 +0900

    20210606

--- Eroon/20210603/DC20210603/EJ2.java
+++ Eroon/20210603/DC20210603/EJ2.java
@@ -9,12 +9,35 @@ public class EJ2 {
 		System.out.println("배열의 크기를 입력하세요.");
 		int n = scan.nextInt();
 		int[][] array= new int[n][n];
-		for(int i = 0; i<n; i++) {
-			for(int j = 0; j<n; j++) {
-				
+		int num = 1;
+		
+		if(1<=n && n<=100) {	//n의 범위
+			
+			for(int i = 0; i < n; i++) { // 세로 줄 
+				if(i%2==0) {	//홀수 줄 (오름차순)
+					for(int j = 0; j < n; j++) { // 가
+						array[i][j] = num;
+						num++;
+					}
+				}
+				else {	//짝수 줄 (내림차순)
+					for(int j = n-1; j >= 0; j--) {
+						array[i][j] = num;
+						num++;
+					}
+				}
 			}
+			
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < n; j++) {
+					System.out.print(array[i][j] + "\t");
+				}
+				System.out.println();
+			}
+				
 		}
-
+		
 	}
 
 }
+
