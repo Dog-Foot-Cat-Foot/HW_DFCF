8a71de4e664aa32293e34174461b5f1a491aaf9a
SH Kim <82145134+shkim-123@users.noreply.github.com>
2021-06-08 21:07:24 +0900

    210608 주석

--- ShKIM/src/day0609/ShQ2.java
+++ ShKIM/src/day0609/ShQ2.java
@@ -14,10 +14,9 @@ public class ShQ2 {
 	// 짝수라면 두글자
 	public static String solution(String s) {
 		String answer = "";
-		int cut = s.length() / 2;
+		int cut = s.length() / 2;	// String 길이 구하기
 
-		System.out.println(cut);
-
+		// substring(시작위치, 끝위치) : String 자르기
 		if (s.length() % 2 != 0) {
 			answer = s.substring(cut, cut + 1);
 		} else {
