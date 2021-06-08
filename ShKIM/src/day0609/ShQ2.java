fb13876dedfb86c58ae3fd580dff70e2200cdb24
SH Kim <82145134+shkim-123@users.noreply.github.com>
2021-06-08 21:04:02 +0900

    210608 2

---
+++ ShKIM/src/day0609/ShQ2.java
@@ -0,0 +1,29 @@
+package day0609;
+
+public class ShQ2 {
+
+	public static void main(String[] args) {
+		String s = "abcde";
+		String s1 = "qwer";
+
+		System.out.println(solution(s));
+		System.out.println(solution(s1));
+	}
+
+	// 단어 s의 가운데 글자 반환
+	// 짝수라면 두글자
+	public static String solution(String s) {
+		String answer = "";
+		int cut = s.length() / 2;
+
+		System.out.println(cut);
+
+		if (s.length() % 2 != 0) {
+			answer = s.substring(cut, cut + 1);
+		} else {
+			answer = s.substring(cut - 1, cut + 1);
+		}
+		return answer;
+	}
+
+}
