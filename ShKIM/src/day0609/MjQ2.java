460822297783857fdfc4128a9746c497d0cc3f2d
SH Kim <82145134+shkim-123@users.noreply.github.com>
2021-06-10 23:36:59 +0900

    210610 2

--- ShKIM/src/day0609/MjQ2.java
+++ ShKIM/src/day0609/MjQ2.java
@@ -1,28 +1,36 @@
 package day0609;
 
+import java.util.*;
+
 public class MjQ2 {
 
 	public static void main(String[] args) {
 
-		System.out.println(solution(12));
+		solution(12);
 
 	}
 
 	// 피보나치 수열: 첫 번째 항 0, 두 번째 항 1, 그 이후의 항은 앞의 두항을 더한값
 	// 입력: 정수 n
 	// 출력: n 이하 까지의 피보나치 수열 출력
-	public static int solution(int n) {
+	public static void solution(int n) {
 		int a = 1;
 		int b = 1;
 		int sum = 0;
+		List<Integer> array = new ArrayList<>();
+		
+		array.add(0);
+		array.add(a);
+		array.add(b);
 
 		for (int i = 3; i < n; i++) {
 			sum = a + b;
 			b = a;
 			a = sum;
+			array.add(sum);
 		}
 
-		return sum;
+		System.out.println(array);
 	}
 
 }
