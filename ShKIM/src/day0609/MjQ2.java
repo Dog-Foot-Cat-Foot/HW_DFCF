3bfe932185825b86e2a973f39935018959b4e208
SH Kim <82145134+shkim-123@users.noreply.github.com>
2021-06-10 23:30:41 +0900

    210610 2

--- ShKIM/src/day0609/MjQ2.java
+++ ShKIM/src/day0609/MjQ2.java
@@ -3,8 +3,26 @@ package day0609;
 public class MjQ2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println(solution(12));
+
+	}
+
+	// 피보나치 수열: 첫 번째 항 0, 두 번째 항 1, 그 이후의 항은 앞의 두항을 더한값
+	// 입력: 정수 n
+	// 출력: n 이하 까지의 피보나치 수열 출력
+	public static int solution(int n) {
+		int a = 1;
+		int b = 1;
+		int sum = 0;
+
+		for (int i = 3; i < n; i++) {
+			sum = a + b;
+			b = a;
+			a = sum;
+		}
+
+		return sum;
 	}
 
 }
