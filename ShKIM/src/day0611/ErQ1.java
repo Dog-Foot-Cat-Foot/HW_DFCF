4b86de7091ea050068de93bc56e2c8fa11e3c525
SH Kim <82145134+shkim-123@users.noreply.github.com>
2021-06-11 14:21:58 +0900

    210611 1

---
+++ ShKIM/src/day0611/ErQ1.java
@@ -0,0 +1,34 @@
+package day0611;
+
+public class ErQ1 {
+
+	public static void main(String[] args) {
+
+		System.out.println(solution(626331));
+
+	}
+
+	// 입력한 수 짝수 : 2로 나눔
+	// 홀수 : 3을 곱하고 1을 더함
+	// 1이 될 때 까지 반복, 500번 반복해도 안나오면 -1 반환
+
+	public static int solution(int num) {
+		int answer = 0;
+		long num2 = num;
+
+		while (num2 != 1) {
+			if (answer == 500) {
+				answer = -1;
+				break;
+			} else if (num2 % 2 == 0) { // 짝수
+				num2 = num2/2;
+			} else if (num2 % 2 != 0) { // 홀수
+				num2 = 3 * num2 + 1;
+			}
+			answer++;
+		}
+
+		return answer;
+	}
+
+}
