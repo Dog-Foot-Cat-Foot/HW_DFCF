a9bc085566222923493b7f957f09f71039f6a6a2
SH Kim <82145134+shkim-123@users.noreply.github.com>
2021-06-11 13:15:28 +0900

    210611 1

--- ShKIM/src/day0611/ShQ1.java
+++ ShKIM/src/day0611/ShQ1.java
@@ -3,8 +3,25 @@ package day0611;
 public class ShQ1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println(solution(987));
 
+	}
+	
+	// 각 자릿수 합을 구하여 리턴
+	
+	// int 길이 구하는 메서드 : (int)(Math.log10(num) + 1)
+	public static int solution(int n) {
+		int answer = 0;
+		int length = (int)(Math.log10(n) + 1); // 자릿수
+		int temp = n;
+		
+		while(temp != 0) {
+			answer += temp%10;
+			temp = temp/10;
+		}
+		
+		return answer;
 	}
 
 }
