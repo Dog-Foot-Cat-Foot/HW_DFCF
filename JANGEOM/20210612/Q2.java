804394d1f05ed9ccde8b60a1d6b10c132db7f2a4
jiean0127 <61142803+jiean0127@users.noreply.github.com>
2021-06-12 20:06:46 +0900

    Update and rename ArrayListTest.java to Q2.java

--- JANGEOM/20210612/Q2.java
+++ JANGEOM/20210612/Q2.java
@@ -2,6 +2,11 @@ package C7;
 import java.util.ArrayList;
 public class ArrayListTest {
 
+	// arraylist를 사용하여 String  으로 선언 후 add 메서드 사용하여 
+	// 안녕하세요 , 반가워요 , 잘있어요. 다시 만나요 문구 출력 
+	// 출력은 for문으로 size와 향상된 for문을 통해 다르게 2번 출력
+	
+	
 	public static void main(String[] args) {
 		
 		
