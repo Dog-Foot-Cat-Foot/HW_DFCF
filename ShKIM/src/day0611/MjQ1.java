1a430e73106421860dc75f342319616a86dd3bef
SH Kim <82145134+shkim-123@users.noreply.github.com>
2021-06-12 18:04:01 +0900

    210612 1

--- ShKIM/src/day0611/MjQ1.java
+++ ShKIM/src/day0611/MjQ1.java
@@ -5,15 +5,18 @@ public class MjQ1 {
 	public static void main(String[] args) {
 		solution(12, 53, 99, 45);
 		solution(2, 75, 25, 50);
+		solution(25, 100, 2, 50);
+		solution(25, 100, 75, 50);
+		solution(25, 100, 75, 98);
 	}
 
 	// 1 <= a,b / c,d <= 100
 	// 교차하면 "cross", 아니면 "not cross"
 
 	public static void solution(int a, int b, int c, int d) {
-		if (a <= c && c <= b && a >= d && b <= d) {
+		if (a < c && c < b && (b < d || d < a)) {
 			System.out.println("cross");
-		} else if (a <= d && d <= b && a <= c && b >= c) {
+		} else if (a < d && d < b && (b < c || c < a)) {
 			System.out.println("cross");
 		} else {
 			System.out.println("not cross");
