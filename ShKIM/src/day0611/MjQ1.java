cebc4264d6b5fade9efc728069c352a888e6853e
SH Kim <82145134+shkim-123@users.noreply.github.com>
2021-06-15 20:04:07 +0900

    210615 1

--- ShKIM/src/day0611/MjQ1.java
+++ ShKIM/src/day0611/MjQ1.java
@@ -1,19 +1,35 @@
 package day0611;
 
+import java.util.Scanner;
+
 public class MjQ1 {
 
 	public static void main(String[] args) {
-		solution(12, 53, 99, 45);
-		solution(2, 75, 25, 50);
-		solution(25, 100, 2, 50);
-		solution(25, 100, 75, 50);
-		solution(25, 100, 75, 98);
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+
+		solution(a, b, c, d);
 	}
 
 	// 1 <= a,b / c,d <= 100
 	// 교차하면 "cross", 아니면 "not cross"
 
 	public static void solution(int a, int b, int c, int d) {
+		int temp;
+
+		if (a > b) {
+			temp = a;
+			a = b;
+			b = temp;
+		} else if (c > d) {
+			temp = c;
+			c = d;
+			d = temp;
+		}
+
 		if (a < c && c < b && (b < d || d < a)) {
 			System.out.println("cross");
 		} else if (a < d && d < b && (b < c || c < a)) {
