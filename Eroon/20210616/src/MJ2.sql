487c00d4729e3e464a2977f400c6f6e04012f0bc
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-06-17 20:09:09 +0900

    Create MJ2.sql

---
+++ Eroon/20210616/src/MJ2.sql
@@ -0,0 +1,16 @@
+elect
+    cus_no
+    , cus_name
+    ,floor((to_number(to_char(sysdate,'YYYY'))
+        - to_number(
+            case
+                substr(jumin_num, 7,1)
+                when '1' then '19'
+                when '2' then '19'
+                else '20'
+                end
+                ||substr(jumin_num,1,2)
+                )+1)*0.1
+                )||'0대'      
+from
+    customer;
