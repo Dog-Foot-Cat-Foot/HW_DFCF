8546496503e1d3a2bbde77846664ae22faeb432b
SH Kim <82145134+shkim-123@users.noreply.github.com>
2021-06-20 13:24:08 +0900

    Add files via upload

---
+++ ShKIM/src/day0621/STUDY_0620.sql
@@ -0,0 +1,14 @@
+-- ¼ÒÈñ
+-- 1
+SELECT COUNT(DISTINCT name)
+FROM animal_ins
+
+-- 2
+SELECT animal_id, name, TO_CHAR(datetime, 'YYYY-MM-DD') ³¯Â¥
+FROM animal_ins
+ORDER BY animal_id
+
+-- 3
+SELECT animal_type, NVL(name, 'No name'), sex_upon_intake
+FROM animal_ins
+ORDER BY animal_id
