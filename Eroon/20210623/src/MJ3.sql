34b848dd2ca7d7af5ae1fbf678356a4807ae83f8
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-06-23 18:07:23 +0900

    Create MJ3.sql

---
+++ Eroon/20210623/src/MJ3.sql
@@ -0,0 +1,10 @@
+select
+	e.emp_no "직원번호"
+	, e.emp_name "직원명"
+	, e.salary "연봉"
+	, s.sal_grade_no "연봉등급"
+from
+	employee e, salary_grade s
+where
+	e.mgr_emp_no in (select e2.emp_no from employee e2 where e2.salary>=3000)
+    and e.salary between s.min_salary and s.max_salary;
