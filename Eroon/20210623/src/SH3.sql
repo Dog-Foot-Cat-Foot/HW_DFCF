f04f37f818b6064384020e1e8c9655aed8026713
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-06-23 23:59:20 +0900

    Create SH3.sql

---
+++ Eroon/20210623/src/SH3.sql
@@ -0,0 +1,8 @@
+select *
+from (select i.NAME, i.DATETIME
+      FROM ANIMAL_INS i LEFT JOIN ANIMAL_OUTS o 
+      ON i.ANIMAL_ID = o.ANIMAL_ID
+      WHERE o.ANIMAL_ID IS NULL
+      ORDER BY i.DATETIME
+     )
+WHERE ROWNUM <= 3;
