d8cd3f068b9b58ab60a5f32a9ea51d3b63dde481
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-06-24 13:09:39 +0900

    Update EJ3.sql

--- Eroon/20210623/src/EJ3.sql
+++ Eroon/20210623/src/EJ3.sql
@@ -2,11 +2,11 @@
 select
 	e.jikup "직급명"
 	, count(distinct e.emp_no) "직급직원수"
-    , count(c.cus_no) "직급담당고객수"
+    	, count(c.cus_no) "직급담당고객수"
 from
 	employee e, customer c
 where
-    e.emp_no = c.emp_no(+)
+    	e.emp_no = c.emp_no(+)
 group by
 	e.jikup;
 
@@ -14,11 +14,11 @@ group by
 select
 	e.jikup "직급명"
 	, count(distinct e.emp_no) "직급직원수"
-    , count(c.cus_no) "직급담당고객수"
+    	, count(c.cus_no) "직급담당고객수"
 from
 	employee e left outer join customer c
 on
-    e.emp_no = c.emp_no
+    	e.emp_no = c.emp_no
 group by
 	e.jikup;
 	
@@ -26,6 +26,6 @@ group by
 select
 	distinct e1.jikup "직급명"
 	, (select count(*) from employee e2 where e1.jikup = e2.jikup) "직급직원수"
-    , (select count(*) from customer c, employee e2 where e1.jikup = e2.jikup and c.emp_no = e2.emp_no) "직급담당고객수"
+    	, (select count(*) from customer c, employee e2 where e1.jikup = e2.jikup and c.emp_no = e2.emp_no) "직급담당고객수"
 from
-    employee e1;
+   	 employee e1;
