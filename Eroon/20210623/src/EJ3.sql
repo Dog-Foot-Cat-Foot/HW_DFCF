65fdf467805c876a8975b661c60024d2550f4dad
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-06-24 13:09:12 +0900

    Update EJ3.sql

--- Eroon/20210623/src/EJ3.sql
+++ Eroon/20210623/src/EJ3.sql
@@ -1,8 +1,8 @@
 -- oracle
 select
 	e.jikup "직급명"
-	, count(e.emp_no) "직급직원수"
-    , count(c.cus_no)
+	, count(distinct e.emp_no) "직급직원수"
+    , count(c.cus_no) "직급담당고객수"
 from
 	employee e, customer c
 where
@@ -13,11 +13,19 @@ group by
 --ANSI
 select
 	e.jikup "직급명"
-	, count(e.emp_no) "직급직원수"
-    , count(c.cus_no)
+	, count(distinct e.emp_no) "직급직원수"
+    , count(c.cus_no) "직급담당고객수"
 from
 	employee e left outer join customer c
 on
     e.emp_no = c.emp_no
 group by
 	e.jikup;
+	
+-- subquery
+select
+	distinct e1.jikup "직급명"
+	, (select count(*) from employee e2 where e1.jikup = e2.jikup) "직급직원수"
+    , (select count(*) from customer c, employee e2 where e1.jikup = e2.jikup and c.emp_no = e2.emp_no) "직급담당고객수"
+from
+    employee e1;
