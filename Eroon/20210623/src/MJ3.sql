8bc20513c350ea0f0c762bc580d9989964b15b9b
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-06-24 15:20:54 +0900

    Update MJ3.sql

--- Eroon/20210623/src/MJ3.sql
+++ Eroon/20210623/src/MJ3.sql
@@ -1,3 +1,4 @@
+-- JOIN
 select
 	e.emp_no "직원번호"
 	, e.emp_name "직원명"
@@ -8,3 +9,15 @@ from
 where
 	e.mgr_emp_no in (select e2.emp_no from employee e2 where e2.salary>=3000)
     and e.salary between s.min_salary and s.max_salary;
+
+-- SUBQUERY
+select
+	e.emp_no "직원번호"
+	, e.emp_name "직원명"
+	, e.salary "연봉"
+	,(select s.sal_grade_no from salary_grade s
+		where e.salary between s.min_salary and s.max_salary) "연봉등급"
+from
+	employee e
+where
+		e.mgr_emp_no in (select e2.emp_no from employee e2 where e2.salary>=3000);
