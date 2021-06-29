fbaa07080ef660af59d1b2d3047831224d29eb3d
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-06-30 00:56:23 +0900

    Create ST.sql

---
+++ Eroon/20210629/src/ST.sql
@@ -0,0 +1,18 @@
+-- 1번 문제 쿼리 실행 순서
+
+SELECT
+    JOB_ID
+    , AVG(SALARY) SAL_AVG
+FROM
+    EMPLOYEES
+WHERE
+    SALARY >13000
+GROUP BY
+    JOB_ID
+HAVING
+    COUNT(*)>1
+ORDER BY SAL_AVG DESC;
+
+--> from -> where -> group by -> having -> select -> order by
+
+-- 2번 문제 
