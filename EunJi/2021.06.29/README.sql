7ba78aa4399880169f7c3bca5b173d080c426df8
eunji03 <84062291+eunji03@users.noreply.github.com>
2021-06-29 14:18:28 +0900

    Create README.sql

---
+++ EunJi/2021.06.29/README.sql
@@ -0,0 +1,33 @@
+## 오라클 이론
+#### 1. join(조인)의 종류 4가지?
+
+<details>
+<summary>답</summary>
+<div markdown="1">       
+ inner join, outer join, self join, cross join
+</div>
+</details>
+
+#### 2. 인라인뷰?
+[ A ]절에 나오는 [ B ]를 말한다.
+[ A ] 절에 [ B ]가 나올 경우 [ C ]로 생각하면 된다.
+
+<details>
+<summary>답</summary>
+<div markdown="1">       
+ A. from  B. select  C. 테이블
+</div>
+</details>
+
+
+
+## sql 문제
+
+#### 1. 고객번호, 고객명, '-'를 포함한 고객주민번호, '-'를 포함한 고객전화번호
+, 담당직원명, 담당직원소속부서명, 담당직원연봉등급
+, 담당직원직속상관명, 담당직원직속상관직급, 직속상관연봉등급을 출력하라.
+단 고객은 다 나와야하고 null은 'X' 로 표시
+
+#### 2. 연봉 서열대로 직원을 검색! 단, 1행부터 15행까지만!
+
+답은 곧...
