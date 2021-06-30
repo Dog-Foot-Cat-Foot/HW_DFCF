c942858f9f580c4ee473299d9b01159ce7cc1aee
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-06-30 22:55:25 +0900

    Update ST.sql

--- Eroon/20210629/src/ST.sql
+++ Eroon/20210629/src/ST.sql
@@ -16,6 +16,25 @@ ORDER BY SAL_AVG DESC;
 --> from -> where -> group by -> having -> select -> order by
 
 -- 2번 문제 
+
 2번
 
 -- 3번 문제
+
+select emp_name "직원명", dep_no "부서번호"
+from employee
+where
+	dep_no in (select dep_no from	employee
+	where	emp_name = '류명한')
+    and emp_name != '류명한';
+    
+-- 4번 문제
+
+select
+	emp_no "직원번호"
+	, emp_name "직원명"
+	, salary "급여"
+from employee
+where
+	dep_no != 10
+	and salary in (select salary from employee where dep_no=10);
