bd0e726749c588821edb87549d2bac4c3f3a81d0
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-07-01 09:17:49 +0900

    Create HW.sql

---
+++ Eroon/20210629/src/HW.sql
@@ -0,0 +1,7 @@
+-- 3번 문제
+
+insert into employee
+	(emp_no ,emp_name, dep_no, jikup, hire_date, jumin_num, phone, mgr_emp_no)
+values
+	( emp_sq.nextval
+	,'김김김', 35, '대리', 3000, '2012-02-28', '8911091109310', '01091499215', 2);
