b5be673089f9eb8dcd0fe3d62ef2b0ef6233c68f
SOHEE <SOHEE@DESKTOP-OAD7JPO>
2021-07-08 21:03:41 +0900

    210708 ch2

--- ShKIM/src/day0709/ch02/Ex01_Variable1.java
+++ ShKIM/src/day0709/ch02/Ex01_Variable1.java
@@ -7,9 +7,9 @@ public class Ex01_Variable1 {
 	// main()은 자바 프로그램의 첫 시작을 나타내는 함수
 	public static void main(String[] args) {
 
-		// level 이라는 이름의 변수를 정수 자료형으로 선언
+		// int형 변수 level 선언
 		// 변수를 선언하면 변수에 값을 넣을 수 있고,
-		// 변수 이름을 사용하여 변수에 들어 있는 데이러를 가져올 수 있다.
+		// 변수 이름을 사용하여 변수에 들어 있는 데이터를 가져올 수 있다.
 		int level;
 		
 		// 선언한 level 변수에 데이터 10을 대입
