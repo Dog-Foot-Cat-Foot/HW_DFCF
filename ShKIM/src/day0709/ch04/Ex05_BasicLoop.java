5bf54f3f2d819424b9c21450c54c18bf5fed186c
SH Kim <82145134+shkim-123@users.noreply.github.com>
2021-07-09 15:19:08 +0900

    210709 ch4

---
+++ ShKIM/src/day0709/ch04/Ex05_BasicLoop.java
@@ -0,0 +1,25 @@
+package day0709.ch04;
+
+public class Ex05_BasicLoop {
+
+	public static void main(String[] args) {
+		
+		// int형 변수 num에 데이터 1 초기화
+		int num = 1;
+		
+		// num = num + 2
+		num += 2;
+		num += 3;
+		num += 4;
+		num += 5;
+		num += 6;
+		num += 7;
+		num += 8;
+		num += 9;
+		num += 10;
+		
+		System.out.println("1부터 10까지의 합은 " + num + "임");
+
+	}
+
+}
