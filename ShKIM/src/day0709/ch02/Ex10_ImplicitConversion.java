f980af7b29ac1e0ff822fb0bf3dd0225e037446c
SH Kim <82145134+shkim-123@users.noreply.github.com>
2021-07-09 10:33:53 +0900

    210709 ch2

--- ShKIM/src/day0709/ch02/Ex10_ImplicitConversion.java
+++ ShKIM/src/day0709/ch02/Ex10_ImplicitConversion.java
@@ -3,7 +3,38 @@ package day0709.ch02;
 public class Ex10_ImplicitConversion {
 
 	public static void main(String[] args) {
-
+		
+		// byte형 변수 bNum 선언과 동시에 데이터 10 대입
+		byte bNum = 10;
+		// int형 변수 iNum 선언과 동시에 bNum의 데이터를 10을 int형으로 자동 형변환하여 대입
+		// 바이트 크기가 작은 자료형에서 큰 자료형으로 대입하는 경우 묵시적 형 변환 일어남
+		// int iNum = (int)10;
+		int iNum = bNum;
+		
+		// byte형 변수 bNum, int형 변수 iNum 데이터 출력
+		System.out.println(bNum); // 출력: 10
+		System.out.println(iNum); // 출력: 10
+		
+		// int형 변수 iNum2 선언과 동시에 데이터 20 대입
+		int iNum2 = 20;
+		// float형 변수 fNum 선언과 동시에 iNum2의 데이터를 20을 float형으로 자동 형변환하여 대입
+		// 덜 정밀한 자료형(int)에서 더 정밀한 자료형(float)으로 대입하는 경우 실수형인 float으로 자동 변환됨
+		// float fNum = (float)20.0;
+		float fNum = iNum2;
+		
+		// int형 변수 iNum2, float형 변수 fNum 데이터 출력
+		System.out.println(iNum2); // 출력: 20
+		System.out.println(fNum); // 출력: 20.0
+		
+		// double형 변수 dNum 선언
+		double dNum;
+		// double형 변수 dNum에 fNum의 데이터 (float)20.0 + iNum의 데이터 (int)10 가
+		// (float)20.0 + (float)10.0 으로 자동 형변환되어 계산된 값 (float)30.0 이
+		// (double)30.0 으로 자동 형 변환되어 dNum에 대입됨
+		dNum = fNum + iNum;
+		// double형 변수 dNum의 데이터 30.0 출력
+		System.out.println(dNum); // 출력: 30.0
+		
 	}
 
 }
---
+++ ShKIM/src/day0709/ch02/Ex11_ExplicitConversion.java
@@ -0,0 +1,35 @@
+package day0709.ch02;
+
+public class Ex11_ExplicitConversion {
+
+	public static void main(String[] args) {
+			
+		// double형 변수 dNum1 선언과 동시에 1.2 데이터 대입
+		double dNum1 = 1.2;
+		// float형 변수 fNum2 선언과 동시에 (float)0.9 데이터 대입
+		float fNum2 = 0.9f;
+		
+		// int형 변수 iNum3 선언과 동시에 
+		// dNum1의 데이터 (double)1.2 가 (int)1 로 형변환
+		// fNum2의 데이터 (float)0.9 가 (int)0 으로 형변환
+		// (int)1 + (int)0 의 데이터 (int)1이 대입됨
+		// 더 정밀한 자료형에서 덜 정밀한 자료형으로 대입하는 경우 형 변환을 명시해야 함
+		// 자료가 손실될 수 있으므로 (이와 같은 경우 소수점 버림)
+		// (int)dNum1를 명시하지 않을 경우 오류 : Type mismatch: cannot convert from double to int
+		// (int)fNum2를 명시하지 않을 경우 오류 : Type mismatch: cannot convert from float to int
+		int iNum3 = (int)dNum1 + (int)fNum2;
+		// int형 변수 iNum4 선언과 동시에
+		// dNum1의 데이터 (double)1.2 와
+		// fNum2의 데이터 (float)0.9 가 (double)0.9 으로 자동 형변환됨
+		// (double)1.2 + (double)0.9 로 연산된 (double)2.1이
+		// (int)2 로 강제 형변환되어 (소수점 버림, 명시적) 대입됨
+		// (int)(dNum1 + fNum2)을 명시하지 않을 경우 오류 : Type mismatch: cannot convert from double to int
+		int iNum4 = (int)(dNum1 + fNum2);
+		
+		// int형 변수 iNum3, iNum4의 데이터 출력
+		System.out.println(iNum3); // 출력: 1
+		System.out.println(iNum4); // 출력: 2
+		
+	}
+
+}
